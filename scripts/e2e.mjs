422354dc8496bccc4e796fabafc055ad3bad8c23
JacopoPatroclo <jacopo.patroclo.martinelli@gmail.com>
2024-02-22 10:22:55 +0100

    fix: remove closewithgrace because it hangs the CI process

--- scripts/e2e.mjs
+++ scripts/e2e.mjs
@@ -1,5 +1,4 @@
 import { exec, execSync } from 'child_process';
-import closeWithGrace from 'close-with-grace';
 
 const watch = process.argv.includes('--watch');
 
@@ -21,9 +20,12 @@ execSync('pnpm build', { shell: true, stdio: 'inherit', env: process.env });
 // Start the dev server
 const devServer = exec('pnpm start', {
   shell: true,
-  stdio: 'inherit',
+  stdio: 'pipe',
   env: process.env,
 });
+
+devServer.stdout.pipe(process.stdout);
+devServer.stderr.pipe(process.stderr);
 
 // Wait for the dev server to be ready
 await new Promise((resolve) => {
@@ -37,33 +39,15 @@ await new Promise((resolve) => {
 // Run the e2e tests
 const cypress = exec(`pnpm cypress ${watch ? 'open' : 'run'}`, {
   shell: true,
-  stdio: 'inherit',
+  stdio: 'pipe',
   env: process.env,
 });
 
 cypress.stdout.pipe(process.stdout);
 cypress.stderr.pipe(process.stderr);
 
-const timeout = setTimeout(
-  () => {
-    console.log('Tests have timed out');
-    process.exit(0);
-  },
-  // If 10 minutes have passed, stop the tests
-  // This is useful for CI environments
-  10 * 60 * 1000,
-);
-
-const clgListener = closeWithGrace({ delay: 500 }, () => {
-  cypress.kill();
-  devServer.kill();
-  clearTimeout(timeout);
-});
-
 cypress.on('exit', (code) => {
   console.log('Tests have finished with code ' + code);
   // Stop the dev server
   devServer.kill();
-  clearTimeout(timeout);
-  clgListener.uninstall();
 });
